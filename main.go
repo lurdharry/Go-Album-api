5bf8d897427f8e097042f5ff29208298e81da638
lurdharry <azeezhammed57@gmail.com>
2023-09-27 19:07:12 -0400

    chore: add formatter

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 	router.ForwardedByClientIP = true
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 
-    router.GET("/albums",getAlbums)
+	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id",getAlbumById)
+	router.GET("/albums/:id", getAlbumById)
 
-    router.Run("localhost:8080")
+	router.Run("localhost:8080")
 }
